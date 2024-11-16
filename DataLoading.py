5d52c606fc66d850bbbf7038f3621e9ee5262649
mapardon <mathieu.pardon@ulb.be>
2024-11-16 22:16:12 +0100

    Implemented data scaling operation

--- DataLoading.py
+++ DataLoading.py
@@ -2,18 +2,21 @@ import pandas as pd
 
 
 class DataLoading:
-    def __init__(self, features_src, labels_src, challenge=False, shuffle=False, short=False):
+    def __init__(self, features_src: str, labels_src: str | None, challenge: bool = False, shuffle: bool = False, short: bool = False):
+        self.short = short
         self.features: pd.DataFrame = pd.read_csv(features_src)
         self.labels: pd.DataFrame | None = None if challenge else pd.read_csv(labels_src)
-        self.short = short
+        self.data_id: pd.DataFrame | None = None
 
         if shuffle:
             self.shuffle_dataset()
 
         if self.short:
             self.features = self.features[:500]
-            if self.labels is not None:
+            if not challenge:
                 self.labels = self.labels[:500]
+
+        self.data_id = self.features.pop("building_id")
 
     def shuffle_dataset(self):
         ds = pd.concat([self.features, self.labels[[self.labels.columns.to_list()[-1]]]], axis="columns")
@@ -23,11 +26,12 @@ class DataLoading:
         # split features/labels and remove id
         self.labels = ds[ds.columns.to_list()[-1]]
         self.features = ds[ds.columns.to_list()[:-1]]
-        if "building_id" in self.features:
-            self.features.pop("building_id")
 
-    def get_dataset(self):
+    def get_train_dataset(self):
         return self.features, self.labels
+
+    def get_challenge_dataset(self):
+        return self.features, self.data_id
 
 
 if __name__ == '__main__':
--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,5 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+from DataAnalysis import FEATURES_SRC, LABELS_SRC
+from DataLoading import DataLoading
+
+CHALLENGE_SRC = "data/test_features.csv"
 
 
 class DataPreprocessing:
-    def __init__(self):
-        pass
+    def __init__(self, challenge: bool = False, short: bool = False):
+
+        self.features: pd.DataFrame | None = None
+        self.labels: pd.DataFrame | None = None
+        self.test_features: pd.DataFrame | None = None
+        self.test_labels: pd.DataFrame | None = None
+        self.data_id: pd.DataFrame | None = None
+
+        if not challenge:
+            dl = DataLoading(FEATURES_SRC, LABELS_SRC, challenge, not challenge, short)
+            self.features, self.labels = dl.get_train_dataset()
+        else:
+            dl = DataLoading(CHALLENGE_SRC, None, challenge, not challenge, short)
+            self.features, self.data_id = dl.get_challenge_dataset()
+
+    def get_train_test_datasets(self):
+        train_features = self.features.iloc[:round(len(self.features) * 0.75), :]
+        train_labels = self.labels[:round(len(self.features) * 0.75)]
+        test_features = self.features.iloc[round(len(self.features) * 0.75):, :]
+        test_labels = self.labels[round(len(self.features) * 0.75):]
+
+        return train_features, train_labels, test_features, test_labels
+
+    def get_challenge_dataset(self):
+        return self.features, self.data_id
+
+    def features_scaling(self, scaler=None):
+        if scaler is not None:
+            if scaler == "minmax":
+                scaler = MinMaxScaler()
+                self.features[self.features.select_dtypes([np.number]).columns] = scaler.fit_transform(self.features.select_dtypes([np.number]))
+
+
+if __name__ == '__main__':
+
+    dp = DataPreprocessing(False, True)
+    print(dp.features.iloc[5])
+    dp.features_scaling("minmax")
+    print(dp.features.iloc[5])
